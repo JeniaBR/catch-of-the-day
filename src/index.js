e1558213c955d10d1dfc659a1000abc10fdedf63
Jenia Brook <jeniabrook@gmail.com>
2017-05-27 17:37:06 +0300

    Add React Router v4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
 
+const Root = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exactly pattern="/" component={StorePicker}/>
+        <Route pattern="/store/:storeId" component={App}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
-
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
