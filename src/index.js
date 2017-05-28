d3fcd517c310992a4afbf8f3e0878663d34ea1be
Jenia Brook <jeniabrook@gmail.com>
2017-05-28 21:58:11 +0300

    Fix router props, used them wrong Add new component for non existing link

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
 const Root = () => {
   return(
     <BrowserRouter>
       <Switch>
-        <Route exactly pattern="/" component={StorePicker}/>
-        <Route pattern="/store/:storeId" component={App}/>
+        <Route path="/" exact component={StorePicker}/>
+        <Route path="/store/:storeId" component={App}/>
+        <Route component={NotFound}/>
       </Switch>
     </BrowserRouter>
   );
