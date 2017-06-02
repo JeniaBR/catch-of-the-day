e6ae51867ed3c16db733116d026784da8d7b5764
Jenia Brook <jeniabrook@gmail.com>
2017-06-02 14:48:34 +0300

    add firebase to the app to sync state

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDwo8UgjS3FwAPjvrl7geT8fyUG9abh2hE",
+  authDomain: "catch-of-the-day-jenia-brook.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-jenia-brook.firebaseio.com"
+});
+
+export default base;
